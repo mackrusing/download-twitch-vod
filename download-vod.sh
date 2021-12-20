14e3c8915b22fb280b280725e19aec50f45fb32c
Mack Rusing <mackrusing@gmail.com>
2021-12-20 00:51:28 -0700

    Finish python script

--- download-vod.sh
+++
@@ -1,9 +0,0 @@
-while getopts i: flag
-do 
-  case "${flag}" in
-    i) id=${OPTARG};;
-  esac
-done
-echo "$id";
-
-twcli api get videos -q id=$id > metadata/$id.json
---
+++ save-vod.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import json
+
+# get arguments
+id = sys.argv[1]
+
+# get current json index file
+json_file = open('metadata/index.json')
+json_data = json.load(json_file)
+json_file.close()
+
+# get vod metadata 
+video_metadata = json.loads(subprocess.run(['twcli', 'api', 'get', 'videos', '-q', f'id={id}'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+json_data[id] = video_metadata['data'][0]
+
+# write to index json file
+with open('metadata/index.json', 'w', encoding='utf-8') as f:
+  json.dump(json_data, f, ensure_ascii=False, indent=2)
+
+# download thumbnail
+thumbnail_url = json_data[id]['thumbnail_url'].replace('%{width}', '1280').replace('%{height}', '720')
+file_type = thumbnail_url.split('.')[-1]
+subprocess.run(['curl', thumbnail_url, '--output', f'thumbnails/{id}.{file_type}'])
+
+# download video
+subprocess.run(['youtube-dl', '-o', '~/Documents/download-twitch-vod/videos/%(id)s.%(ext)s', f'https://www.twitch.tv/videos/{id}'])
